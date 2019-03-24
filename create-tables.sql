bdcaf9184dbf537d6301569d4d4c3fbf9975ff3c
Jos de Poorter <45604472+jampoort@users.noreply.github.com>
2019-03-24 13:02:00 +0100

    LOGIN
    TOEGEVOEGD DE LOGIN PHP

--- create-tables.sql
+++ create-tables.sql
@@ -64,6 +64,17 @@ CREATE TABLE `ORDER` (
     ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
+CREATE TABLE `users` 
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(50) NOT NULL,
+     `last_name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `hash` VARCHAR(32) NOT NULL,
+    `active` BOOLEAN NOT NULL DEFAULT 0,
+PRIMARY KEY (`id`) 
+);
 
 /*
 ===========================
